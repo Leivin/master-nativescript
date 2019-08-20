ac5fdab35995395d1bc790cb1a03d29533005a30
Dominika Kawucha <dominika.kawucha@icloud.com>
2019-08-20 22:27:14 +0200

    Dodanie szkieletu wysuwanego menu

--- src/app/app.module.tns.ts
+++ src/app/app.module.tns.ts
@@ -1,5 +1,6 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
+import { NativeScriptUISideDrawerModule } from "nativescript-ui-sidedrawer/angular";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,6 +21,7 @@ import { HomeComponent } from './home/home.component';
   imports: [
     NativeScriptModule,
     AppRoutingModule,
+    NativeScriptUISideDrawerModule
   ],
   providers: [],
   bootstrap: [AppComponent],
